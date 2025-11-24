1feae17d0eb5a929d88de5d07fd4f12952636e86
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-24 23:18:48 +0300

    add-item-method

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -6,3 +6,11 @@ class CashRegister:
         self.total = 0.0
         self.items = []
         self.last_transaction = None
+
+    def add_item(self, title, price, quantity=1):
+        amount = price * quantity
+        self.total += amount
+        self.items.extend([title] * quantity)
+        self.last_transaction = {'amount': amount, 'count': quantity}
+
+   
